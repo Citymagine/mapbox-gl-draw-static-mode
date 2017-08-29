16504d91b35f4387de58501b6a727bd7b20bdb04
Cyril CHAPON <cyril@citymagine.com>
2017-08-29 11:11:45 +0200

    Export for different envs, including browser

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var StaticMode = module.exports = {};
-
-StaticMode.onSetup = function() {
-  this.setActionableState(); // default actionable state is false for all actions
-  return {};
-};
-
-StaticMode.toDisplayFeatures = function(state, geojson, display) {
-  display(geojson);
-};
+(function (root, factory) {
+  if ( typeof define === 'function' && define.amd ) {
+    define('something', factory(root));
+  } else if ( typeof exports === 'object' ) {
+    module.exports = factory(root);
+  } else {
+    root.something = factory(root);
+  }
+})(window || this, function (root) {
+  return {
+    onSetup: function() {
+      this.setActionableState();
+      return {};
+    },
+    toDisplayFeatures: function(state, geojson, display) {
+      display(geojson);
+    }
+  }
+});
